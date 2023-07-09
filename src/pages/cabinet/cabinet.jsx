93aa4f9e279775ce0e5a59f65e4b93c20afa8ac5
Marina Gutkevich <gutmalina@mail.ru>
2023-07-09 19:23:11 +0300

    fix button link router

--- src/pages/cabinet/cabinet.jsx
+++ src/pages/cabinet/cabinet.jsx
@@ -5,13 +5,43 @@ import { getUser } from '../../store/auth/selectors'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { DesktopDatePicker } from '@mui/x-date-pickers'
-import { TEXT_REPORT_HISTORY } from '../../utils/constants'
+import {
+  TEXT_REPORT_HISTORY,
+  TYPE_INPUT_CLIENT_ID,
+  TYPE_INPUT_CLIENT_SECRET,
+  TYPE_BTN_SAVE,
+  TYPE_BTN_EDIT
+} from '../../utils/constants'
 import TableCabinet from '../../components/table-cabinet/table-cabinet'
+import SimpleInputElement from '../../components/simple-input-element/simple-input-element'
+import { useDispatch } from 'react-redux'
+import { addAdvCabinet } from '../../store/auth'
+import ButtonElement from '../../components/button-element/button-element'
 
 const Cabinet = () => {
+  const dispatch = useDispatch()
+  const [textButtonSubmit, setTextButtonSubmit] = useState(TYPE_BTN_SAVE)
   const user = useSelector(getUser)
   const [startDate, setStartDate] = useState(new Date())
   const [endData, setEndDate] = useState(new Date())
+  const [credentials, setCredentials] = useState({
+    [TYPE_INPUT_CLIENT_ID]: '',
+    [TYPE_INPUT_CLIENT_SECRET]: ''
+  })
+
+  const onChange = (e) => {
+    e.preventDefault()
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    dispatch(addAdvCabinet(credentials))
+    setTextButtonSubmit(TYPE_BTN_EDIT)
+  }
 
   return (
     <>
@@ -23,6 +53,23 @@ const Cabinet = () => {
           </div>
           <p className={styles.user_info_second_row}>{user.email}</p>
         </div>
+        <form className={styles.client_credentials} onSubmit={handleFormSubmit}>
+          <SimpleInputElement
+            type={TYPE_INPUT_CLIENT_ID}
+            value={credentials[TYPE_INPUT_CLIENT_ID]}
+            onChange={(e) => {
+              onChange(e)
+            }}
+          />
+          <SimpleInputElement
+            type={TYPE_INPUT_CLIENT_SECRET}
+            value={credentials[TYPE_INPUT_CLIENT_SECRET]}
+            onChange={(e) => {
+              onChange(e)
+            }}
+          />
+          <ButtonElement type={textButtonSubmit}></ButtonElement>
+        </form>
         <h3 className={styles.report_history}>{TEXT_REPORT_HISTORY}</h3>
         <div className={styles.report_history_date}>
           <div className={styles.date_pickers}>
